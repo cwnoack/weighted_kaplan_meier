bc77f1f3bf5d9e7dd7a95af2c59a4a1d11b0ae42
Clint Noack <clint.noack@gmail.com>
2015-04-07 13:49:46 -0400

    Test upload to github

---
+++ load_dependencies.R
@@ -0,0 +1,14 @@
+load_dependencies <- function(pkgs = list('plyr','dplyr',
+                                          'ggplot2','ggthemes')){
+  is.installed <- function(pkg){
+    if(!(require(pkg, character.only = T, quietly = T, warn.conflicts = F))){
+      install.packages(pkg, verbose = F)
+      library(pkg, quietly = T, verbose = F, warn.conflicts = F)
+    }
+  }
+  
+  lapply(pkgs, is.installed)
+  pkg_string <- paste(unlist(pkgs), sep = '', collapse = ', ')
+  cat('The following packages have been loaded: ',pkg_string,'.\n',
+      sep = '')
+}

3f8e6b006ab961f9c762f40e37071ae01fef3cb9
Clint Noack <clint.noack@gmail.com>
2015-04-08 14:39:47 -0400

    Start-up file will not self-reference or run MWE.

--- run_at_startup_to_load.R
+++ run_at_startup_to_load.R
@@ -1,7 +1,7 @@
 # Source this file at Rstudio start up to load everything
 # Hopefully this will be unecessary once I have the package built.
 all_files <- list.files()
-all_files <- all_files[all_files != 'run_at_startup_to_load.R']
+all_files <- all_files[!(all_files %in% c('run_at_startup_to_load.R', 'MWE_for_logrank.R'))]
 files_to_run <- grep(x = all_files, pattern = '.*\\.R$')
 sapply(files_to_run, function(i) source(all_files[i]))
 load_dependencies()

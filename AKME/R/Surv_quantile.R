8aadd2feb1a5afbdd23ed7936c6cafd531397fe8
Clint Noack <clint.noack@gmail.com>
2015-04-21 16:09:21 -0400

    Added documentation and namespaces.

--- AKME/R/Surv_quantile.R
+++ AKME/R/Surv_quantile.R
@@ -1,11 +1,29 @@
-# Function for calculating quantiles of distributions using AKME
+#' Weighted quantiles for censored data
+#' 
+#' Calculates the AKME for censored data and returns a table of desired quantiles. 
+#'  Output contains the desired percentiles and the corresponding order statistics.
+#'  Code will throw warnings if one or more of the desired percentiles is less than 
+#'  the fraction of censored data and may produce NA values for order statistics at 
+#'  those percentiles depending on the `type` argument to the function 
+#'  (i.e. `nearest` will not return NA but may return repeated values).
+#' @param censored_data data.frame with three columns: (1) measured concentration,
+#'  (2) flag for nondetects [BDL = 1], and
+#'   (3) unique site identifier
+#' @param percentiles vector of desired percentiles
+#' @param type method for calculation of quantiles, either: 
+#' (1) 'interp' for interpolation between nearest points,
+#' (2) 'PiR' for nearest value that does not exceed the percentile, or
+#' (3) 'nearest' for nearest value (that may exceed desired percentile)
+#' @param sig.fig desired number of significant figures for estimate.
+#' @return perc_df data.frame containing the desired percentiles and the 
+#' order statistic estimates of those percentiles from `censored_data`
 Surv_quantile <- function(censored_data,
                           percentiles = c(0.05,0.1,0.25,0.5,0.75,0.9,0.95),
                           type = 'interp',
                           sig.fig = 3){
   cenfrac <- mean(as.logical(censored_data$Censored))
   
-  weighted_km <- Surv_weighted(censored_data) %>% select(Concentration, S)
+  weighted_km <- Surv_weighted(censored_data) %>% dplyr::select(Concentration, S)
   
   if(any(cenfrac > percentiles)){
     warning(paste('The fraction of censored data is larger ',
@@ -33,11 +51,12 @@ Surv_quantile <- function(censored_data,
                                         y = weighted_km[c(h.low, h.high),'Concentration'],
                                         xout = Percentile)$y)},
          PiR = {perc_df <- perc_df %>%
-                  mutate(Xh = weighted_km[h.low,'Concentration'])},
+                  dplyr::mutate(Xh = weighted_km[h.low,'Concentration'])},
          nearest = {perc_df <- perc_df %>%
-                      mutate(Xh = weighted_km[h.low,'Concentration'])})
+                      dplyr::mutate(Xh = weighted_km[h.low,'Concentration'])})
   
-  perc_df <- perc_df %>% arrange(desc(Percentile)) %>% select(Percentile, Xh) %>%
-    mutate(Xh = signif(Xh, sig.fig))
+  perc_df <- perc_df %>% dplyr::arrange(desc(Percentile)) %>% 
+    dplyr::select(Percentile, Xh) %>%
+    dplyr::mutate(Xh = signif(Xh, sig.fig))
   return(perc_df)
 }
--- AKME/R/grp_quantiles.R
+++ AKME/R/grp_quantiles.R
@@ -1,10 +1,26 @@
-# Calculate KM quantiles
+#' Weighted quantiles for grouped data
+#' 
+#' Calculates the AKME for grouped data and returns a table of desired quantiles.
+#'  Requires the same input as `Surv_quanitle` but with an additional column:
+#'   "Dataset". Output will be same as `Surv_quantile` but for each group.
+#' @param grouped_data data.frame with four columns: (1) measured concentration,
+#'  (2) flag for nondetects [BDL = 1],
+#'   (3) unique site identifier,
+#'    (4) dataset identifier.
+#' @param percentiles vector of desired percentiles
+#' @param type method for calculation of quantiles, either: 
+#' (1) 'interp' for interpolation between nearest points,
+#' (2) 'PiR' for nearest value that does not exceed the percentile, or
+#' (3) 'nearest' for nearest value (that may exceed desired percentile)
+#' @param sig.fig desired number of significant figures for estimate.
+#' @return quantile_tbl data.frame containing the desired percentiles and the 
+#' order statistic estimates of those percentiles from each group in `grouped_data`
 grp_quantiles <- function(grouped_data,
                           percentiles = c(0.05,0.1,0.25,0.5,0.75,0.9,0.95),
-                          type = 'interp'){
+                          type = 'interp', sig.fig = 3){
   quantile_tbl <-grouped_data %>% plyr::ddply(.(Dataset), function(df){
     Surv_quantile(censored_data = dplyr::select(df, -Dataset),
-                  percentiles = percentiles, type = type)
+                  percentiles = percentiles, type = type, sig.fig = sig.fig)
   }) %>% tidyr::spread(Dataset, Xh) %>% dplyr::arrange(desc(Percentile))
   return(quantile_tbl)
 }
